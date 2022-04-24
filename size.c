3c8672a647a9e0cea97212948a1df6503faeb4a7
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-04-23 23:39:56 -0800

    Create size.c

---
+++ size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{ 
+  printf("int is    %2d bytes \n", sizeof(short int));
+  printf("long int is  %2d bytes \n", sizeof(long int));
+  printf("float is    %2d bytes \n", sizeof(float));
+  printf("double is   %2d bytes \n", sizeof(double));
+  printf("long double is  %2d bytes \n", sizeof(long double));
+  printf("char is   %2d bytes \n", sizeof(char));
+  return 0;
+
+} 
