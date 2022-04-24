d697ddedaf931f374e8e435fba608b346d782bf6
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-04-24 00:10:09 -0800

    Create natural-numbers.c

---
+++ natural-numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+
+    int n,sum=0;
+    
+    while(1)
+    {
+ printf("\nEnter The Natural Number :\n");
+    scanf("%d",&n);
+    
+ sum=(n*(n+1))/2;
+ 
+ printf("\nSum Of Natural Number Is  = %d\n",sum);
+    
+ }
+ return 0;
+}
