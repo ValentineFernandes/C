3f1323654f6aa6ce301879c0dfbe0f1ebfcbe416
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-04-23 23:38:37 -0800

    Create temperature.c

---
+++ temperature.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+ float cel, fah;
+
+ printf("\nEnter Temperature in Celsius : ");
+ scanf("%f", &cel);
+
+ fah = (1.8 * cel) + 32;
+ printf("\nTemperature in Fahrenheit : %f ", fah);
+
+ return (0);
+
+}
