aa9ce195360a9267fe808002fb33db42384bc032
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-04-23 23:34:00 -0800

    Create simple-interest.c

---
+++ simple-interest.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+   
+   float amount, rate, time, si;
+
+   printf("\nEnter Principal Amount : ");
+   scanf("%f", &amount);
+
+   printf("\nEnter Rate of Interest : ");
+   scanf("%f", &rate);
+
+   printf("\nEnter Period of Time   : ");
+   scanf("%f", &time);
+
+   si = (amount * rate * time) / 100;
+   printf("\nSimple Interest : %f", si);
+
+   return(0);
+}
