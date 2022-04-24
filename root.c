6a4515a5f863be2525495184a3e66e67d688c17d
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-04-24 01:18:43 -0800

    Create root.c

---
+++ root.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main(){
+
+    long int sum,num,m;
+
+    printf("Enter any number: \n");
+    scanf("%ld",&num);
+
+    while( num > 10 )
+ {
+        sum = 0;
+
+        while(num)
+  {
+            m = num%10;
+            num = num/10;
+            sum += m;
+        }
+        if(sum > 10)
+            num = sum;
+        else
+            break;
+    }
+
+    printf("Generic Root is: %ld\n",sum);
+
+return 0;
+
+}
