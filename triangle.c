143ea0b2d13f19c7ebd90a3c22539527c4ae7a84
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-04-23 23:31:17 -0800

    Create triangle.c

---
+++ triangle.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    /*Program By Ghanendra Yadav
+    Visit http://www.programmingwithbasics.com/
+    */
+    float b,h,area;
+
+    printf("Enter Height and Base Of Triangle : ");
+    scanf("%f %f",&h,&b);
+
+    area = (h*b)/2;
+    printf("Area of Triangle is: %f\n",area);
+    return 0;
+}
