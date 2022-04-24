7e7bc896d0d6049fa48b53a50ea3d18b1b4e7744
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-04-24 00:03:28 -0800

    Create leap-year.c

---
+++ leap-year.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() 
+{
+    int year;
+
+    printf("Enter a Year: ");
+    scanf("%d", & year);
+
+    if (year % 4 == 0) 
+	{
+        if (year % 100 == 0) 
+		{
+            if (year % 400 == 0)
+                printf("%d is a Leap Year.", year);
+            else
+                printf("%d is Not a Leap Year.", year);
+        } else
+            printf("%d is a Leap Year.", year);
+    } 
+    else
+        printf("%d is Not a Leap Year.", year);
+    return 0;
+}
