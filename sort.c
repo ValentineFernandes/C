9b6e710640e1b6b179743ed61c24dbde3f28243d
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-04-24 05:13:52 -0800

    Update sort.c

--- sort.c
+++ sort.c
@@ -1,21 +1,15 @@
 #include<stdio.h>
-
 main()
 {
 int i,n=11,j;
 float temp=0.0f;
-
 float a[11] = {1.01,6.66,3.3,7.5,2.2,4.1,7.9,9.7,0.01,9.14,0.69};
-
 printf("\nSorting A Float Array \n");
-
 printf("\n\nElement Before Sorting \n\n");
-
 for(i= 0; i<=10; i++) 
 {
     printf("%.2f  ", a[i]);
 }
-
 printf("\n");
 
 printf("\n\nElement After Sorting \n\n");
