1bff32188d56a90d05876d18447df7fae6ca59c1
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-04-24 00:02:31 -0800

    Create even-odd.c

---
+++ even-odd.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+ int num;
+
+ printf("\nEnter Any Number To Check Even Or Odd :\n");
+ scanf("%d", &num);
+
+ if (num%2 == 0)
+ {
+     printf("%d is EVEN\n", num);
+ }
+ else
+ {
+     printf("%d is ODD\n", num);
+ }
+ return 0;
+}
