a6ceb4acdf6b02570fb63ae41e433058b38391f1
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-04-23 23:36:21 -0800

    Create gross-salary.c

---
+++ gross-salary.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+{
+  
+    float GrossPayment,basic,da,hra,da1,hra1;
+
+    printf("Enter basic salary : ");
+    scanf("%f", &basic);
+
+   
+    printf("Enter DA : ");
+    scanf("%f", &da1);
+
+    printf("Enter HRA : ");
+    scanf("%f", &hra1);
+
+
+    da = (da1 * basic) / 100;
+    hra = (hra1 * basic) / 100;
+
+    GrossPayment = basic + da + hra;
+
+    printf("\nGross Salary :%f\n",GrossPayment);
+    return (0);
+}
