b9d351577f6edb4ce8feea50dd48d3cf678a2609
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-04-23 23:44:53 -0800

    Create integer.c

---
+++ integer.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+  int num;
+  printf("\nEnter The Number .\n");
+  scanf("%d",&num);
+  printf("\nOutput Is\n\n");
+  printf("%d  ,%d  ,%d \n\n",num,num*num,num*num*num);
+  return 0;
+}
