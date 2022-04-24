04a5f31a3e828ba40e71844e8214fab4859b18f9
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-04-23 23:50:50 -0800

    Create power.c

---
+++ power.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+  
+  int num,a,b,c;
+  printf("\nEnter The Number .\n");
+  scanf("%d",&num);
+  a=pow(num,1);
+  b=pow(num,2);
+  c=pow(num,3);
+  printf("\nOutput Is\n\n");
+  printf("%d  ,%d  ,%d \n\n",a,b,c);
+  return 0;
+}
