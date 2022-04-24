a9e61bbf64a4fa4687f7351c64fce59808715bb1
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-04-23 23:53:59 -0800

    Create percentage-marks.c

---
+++ percentage-marks.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+   int s1, s2, s3, s4, s5, sum, total = 500;
+   float per;
+
+   printf("\nEnter marks of 5 subjects : ");
+   scanf("%d %d %d %d %d", &s1, &s2, &s3, &s4, &s5);
+
+   sum = s1 + s2 + s3 + s4 + s5;
+   printf("\nSum : %d", sum);
+
+   per = (sum * 100) / total;
+   printf("\nPercentage : %f", per);
+
+   return (0);
+
+}
