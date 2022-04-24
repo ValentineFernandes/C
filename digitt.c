af845e230b0204ba9c07dd29524e5fd8d7c3ac04
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-04-24 01:17:53 -0800

    Create digitt.c

---
+++ digitt.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+  
+   int n,count=0;
+  
+   printf("Enter Any Number To Count Digit : \n\n");
+   scanf("%d", &n);
+  
+   count=log10(n)+1;
+   
+   printf("\nNumber of Digits Is = %d",count);
+ return 0;
+}
